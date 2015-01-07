14b4b2bd2e147b607086abdd42191f1b14bb730e
gaudiauj <gaudiauj@gmail.com>
2015-01-07 06:14:49 +0100

    php doc

--- photo malin v2/Library/user.php
+++ photo malin v2/Library/user.php
@@ -18,11 +18,18 @@ session_start();
 class User extends ApplicationComponent
 {
 
+    /**
+     * @param $attr
+     * @return null
+     */
     public function getAttribute($attr)
     {
         return isset($_SESSION[$attr]) ? $_SESSION[$attr] : null;
     }
 
+    /**
+     * @return mixed
+     */
     public function getFlash()
     {
         $flash = $_SESSION['flash'];
@@ -31,21 +38,34 @@ class User extends ApplicationComponent
         return $flash;
     }
 
+    /**
+     * @return bool
+     */
     public function hasFlash()
     {
         return isset($_SESSION['flash']);
     }
 
+    /**
+     * @return bool
+     */
     public function isAuthenticated()
     {
         return isset($_SESSION['auth']) && $_SESSION['auth'] === true;
     }
 
+    /**
+     * @param $attr
+     * @param $value
+     */
     public function setAttribute($attr, $value)
     {
         $_SESSION[$attr] = $value;
     }
 
+    /**
+     * @param bool $authenticated
+     */
     public function setAuthenticated($authenticated = true)
     {
         if (!is_bool($authenticated))
@@ -56,6 +76,9 @@ class User extends ApplicationComponent
         $_SESSION['auth'] = $authenticated;
     }
 
+    /**
+     * @param $value
+     */
     public function setFlash($value)
     {
         $_SESSION['flash'] = $value;
