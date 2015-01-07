83a03fdc4ef78e308adcbf30f95f3a7cb88d3f64
gaudiauj <gaudiauj@gmail.com>
2015-01-07 06:18:29 +0100

    php doc

--- photo malin v2/Library/Route.php
+++ photo malin v2/Library/Route.php
@@ -16,6 +16,12 @@ class Route
     protected $varsNames;
     protected $vars = array();
 
+    /**
+     * @param $url
+     * @param $module
+     * @param $action
+     * @param array $varsNames
+     */
     public function __construct($url, $module, $action, array $varsNames)
     {
         $this->setUrl($url);
@@ -24,11 +30,18 @@ class Route
         $this->setVarsNames($varsNames);
     }
 
+    /**
+     * @return bool
+     */
     public function hasVars()
     {
         return !empty($this->varsNames);
     }
 
+    /**
+     * @param $url
+     * @return bool
+     */
     public function match($url)
     {
         if (preg_match('`^' . $this->url . '$`', $url, $matches))
@@ -40,6 +53,9 @@ class Route
         }
     }
 
+    /**
+     * @param $action
+     */
     public function setAction($action)
     {
         if (is_string($action))
@@ -48,6 +64,9 @@ class Route
         }
     }
 
+    /**
+     * @param $module
+     */
     public function setModule($module)
     {
         if (is_string($module))
@@ -56,6 +75,9 @@ class Route
         }
     }
 
+    /**
+     * @param $url
+     */
     public function setUrl($url)
     {
         if (is_string($url))
@@ -64,31 +86,49 @@ class Route
         }
     }
 
+    /**
+     * @param array $varsNames
+     */
     public function setVarsNames(array $varsNames)
     {
         $this->varsNames = $varsNames;
     }
 
+    /**
+     * @param array $vars
+     */
     public function setVars(array $vars)
     {
         $this->vars = $vars;
     }
 
+    /**
+     * @return mixed
+     */
     public function action()
     {
         return $this->action;
     }
 
+    /**
+     * @return mixed
+     */
     public function module()
     {
         return $this->module;
     }
 
+    /**
+     * @return array
+     */
     public function vars()
     {
         return $this->vars;
     }
 
+    /**
+     * @return mixed
+     */
     public function varsNames()
     {
         return $this->varsNames;
